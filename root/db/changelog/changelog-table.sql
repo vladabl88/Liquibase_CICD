329f75186c9bca23e872790033f86c3e75345174
Vladimir Blažević <vladimir.blazevic@clarivate.com>
2025-02-12 12:09:03 +0100

    test

--- root/db/changelog/changelog-table.sql
+++ root/db/changelog/changelog-table.sql
@@ -12,3 +12,20 @@ create or replace TABLE SASDEV.SANDBOX.TEST_TABLE_CICD (
 );
 --rollback drop table SASDEV.SANDBOX.TEST_TABLE_CICD
 
+--changeset Vladimir.Blazevic:4 labels:TEST context:Testing of update to tag sql tag:version_1.0
+--comment: example comment
+alter table SASDEV.SANDBOX.TEST_TABLE_CICD add column country varchar(2);
+--rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
+
+--changeset Vladimir.Blazevic:5 labels:TEST tagDatabase:testtag
+--comment: Testing of update to tag sql
+alter table SASDEV.SANDBOX.TEST_TABLE_CICD drop column country;
+--rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
+
+--changeset Vladimir.Blazevic:6 labels:TEST tagDatabase:testtag
+--comment: Testing of update to tag sql
+alter table SASDEV.SANDBOX.TEST_TABLE_CICD add column country varchar(2);
+--rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
+
+--changeset liquibase-docs:tagDatabase-example
+--tagDatabase: 'version_1.3'
