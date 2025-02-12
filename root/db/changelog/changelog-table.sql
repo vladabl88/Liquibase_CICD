c63f2863ee1f24bae6a3797b01d7a60fa6cdb48c
Vladimir Blažević <vladimir.blazevic@clarivate.com>
2025-02-12 12:14:45 +0100

    test 3

--- root/db/changelog/changelog-table.sql
+++ root/db/changelog/changelog-table.sql
@@ -12,17 +12,17 @@ create or replace TABLE SASDEV.SANDBOX.TEST_TABLE_CICD (
 );
 --rollback drop table SASDEV.SANDBOX.TEST_TABLE_CICD
 
---changeset Vladimir.Blazevic:4 labels:TEST context:Testing of update to tag sql tag:version_1.0
---comment: example comment
+--changeset Vladimir.Blazevic:4 labels:TEST context:New_function
+--comment: We are testing github actions
 alter table SASDEV.SANDBOX.TEST_TABLE_CICD add column country varchar(2);
 --rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
 
---changeset Vladimir.Blazevic:5 labels:TEST tagDatabase:testtag
+--changeset Vladimir.Blazevic:5 labels:TEST
 --comment: Testing of update to tag sql
 alter table SASDEV.SANDBOX.TEST_TABLE_CICD drop column country;
---rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
+--rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD add COLUMN country varchar(2);
 
---changeset Vladimir.Blazevic:6 labels:TEST tagDatabase:testtag
+--changeset Vladimir.Blazevic:6 labels:PROD context:New_function_2
 --comment: Testing of update to tag sql
 alter table SASDEV.SANDBOX.TEST_TABLE_CICD add column country varchar(2);
 --rollback ALTER TABLE SASDEV.SANDBOX.TEST_TABLE_CICD DROP COLUMN country;
