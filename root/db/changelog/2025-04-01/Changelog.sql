a8ede9b88a3f2317b4a7f3eafec60b1edb433007
Vladimir Blazevic <vladabl88@gmail.com>
2025-04-04 11:06:07 +0200

    Test for Demonstration

---
+++ root/db/changelog/2025-04-01/Changelog.sql
@@ -0,0 +1,69 @@
+--liquibase formatted sql
+
+--changeset Vladimir.Blazevic:1 labels:2025-04-01 context:DEV
+--comment: Test comment for CICD5
+create TABLE PUBLIC.TEST_TABLE_CICD5 (
+	TEST_COLUMN varchar(100),
+	TEST_DATE DATETIME
+);
+--rollback drop table PUBLIC.TEST_TABLE_CICD5;
+
+--changeset Vladimir.Blazevic:2 labels:2025-04-01 context:TEST
+--comment: Test comment for CICD5
+create or replace TABLE PUBLIC.TEST_TABLE_CICD5 (
+	TEST_COLUMN varchar(100),
+	TEST_DATE DATETIME
+);
+--rollback drop table PUBLIC.TEST_TABLE_CICD5;
+
+--changeset vladimir.blazevic:3 labels:2025-04-01 context:DEV endDelimiter:/
+--comment: Test Test comment for CICD5 Procedure
+CREATE OR REPLACE PROCEDURE PUBLIC.TEST_PROCEDURE5()
+RETURNS varchar
+LANGUAGE SQL
+AS
+$$
+INSERT INTO PUBLIC.TEST_TABLE_CICD5
+SELECT 'Hello CICD', CURRENT_TIMESTAMP(2);
+$$
+;
+/
+
+--rollback DROP PROCEDURE PUBLIC.TEST_PROCEDURE4();
+
+--changeset vladimir.blazevic:4 labels:2025-04-01 context:TEST endDelimiter:/
+--comment: Test Test comment for CICD5 Procedure
+CREATE OR REPLACE PROCEDURE PUBLIC.TEST_PROCEDURE5()
+RETURNS varchar
+LANGUAGE SQL
+AS
+$$
+INSERT INTO PUBLIC.TEST_TABLE_CICD5
+SELECT 'Hello CICD', CURRENT_TIMESTAMP(2);
+$$
+;
+/
+
+--rollback DROP PROCEDURE PUBLIC.TEST_PROCEDURE4();
+
+--changeset Vladimir.Blazevic:5 labels:2025-04-01 context:DEV
+--comment: Testing of CICD4
+ALTER TABLE PUBLIC.TEST_TABLE_CICD4 ADD COLUMN TEST_COLUMN3 VARCHAR(300);
+--rollback ALTER TABLE PUBLIC.TEST_TABLE_CICD4 DROP COLUMN TEST_COLUMN3;
+
+--changeset Vladimir.Blazevic:6 labels:2025-04-01 context:TEST
+--comment: Testing of CICD4
+ALTER TABLE PUBLIC.TEST_TABLE_CICD4 ADD COLUMN TEST_COLUMN3 VARCHAR(300);
+--rollback ALTER TABLE PUBLIC.TEST_TABLE_CICD4 DROP COLUMN TEST_COLUMN3;
+
+--changeset Vladimir.Blazevic:7 labels:2025-04-01 context:DEV
+--comment: Testing of Data insert through CI/CD process
+Insert into PUBLIC.TEST_TABLE_CICD5
+SELECT 'Testing of Data insert through CI/CD process', CURRENT_TIMESTAMP(2);
+--rollback DELETE FROM PUBLIC.TEST_TABLE_CICD5 TEST_COLUMN='Testing of Data insert through CI/CD process';
+
+--changeset Vladimir.Blazevic:8 labels:2025-04-01 context:TEST
+--comment: Testing of Data insert through CI/CD process
+Insert into PUBLIC.TEST_TABLE_CICD5
+SELECT 'Testing of Data insert through CI/CD process', CURRENT_TIMESTAMP(2);
+--rollback DELETE FROM PUBLIC.TEST_TABLE_CICD5 WHERE TEST_COLUMN='Testing of Data insert through CI/CD process';
